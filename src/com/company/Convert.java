138d9ca51c3ff8c6b1016ba834782e6e17151662
jb838 <jacob.blasek1220@gmail.com>
2019-01-29 11:02:13 -0500

    oh yeah yeah

---
+++ src/com/company/Convert.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Convert {
+
+    public static String rgbToHex(int r, int g, int b){
+
+
+
+    //MODULO
+
+        return "yeah";
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner jbReader = new Scanner(System.in);
+        System.out.println("DUUUUDDE WHAT YOU TRYNA CONVERT THO???. (Enter a number 0 - 255 on 3 separate lines.)");
+
+        int r = jbReader.nextInt();
+        int g = jbReader.nextInt();
+        int b = jbReader.nextInt();
+
+        System.out.println("Here is the value in hex");
+        Convert.rgbToHex(r, g, b);
     }
 }
